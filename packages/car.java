eb825c1969ed5a3d1f18874822e3fc8690681cbe
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2026-05-14 01:31:13 +0530

    add code

---
+++ packages/car.java
@@ -0,0 +1,12 @@
+package packages;
+
+public class car extends vehicle {
+    public String ms;
+
+    public String getMs() {
+        return this.ms;
+    }
+    public void setMs(String ms){
+        this.ms = ms;
+    }
+}
---
+++ packages/vehicle.java
@@ -0,0 +1,11 @@
+package packages;
+
+public class vehicle {
+    public int rno;
+    public int getRno() {
+        return this.rno;
+    }
+    public void setRno(int rno){
+        this.rno = rno;
+    }
+}
